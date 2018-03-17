6013918b10cd220b2bc5cf2fd1b7a4890a3681e2
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-03-16 22:43:43 -0700

    Added console log

--- FileServer.js
+++ FileServer.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
   fs.readFile('SamplePage.html', function(err, data) {
+    console.log("Open http://lcoalhost:8080 on your browser.");
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
